8094e33d143cc9736319da37bc6ed49cd39dadc2
Javier Guerrero <javigh87@gmail.com>
2023-11-12 08:48:46 +0100

    styles: SelectionElementContainer styles responsive

--- src/components/SelectionElement/SelectionElementContainer.tsx
+++ src/components/SelectionElement/SelectionElementContainer.tsx
@@ -11,7 +11,7 @@ const SelectionElementContainer: React.FC<Props> = ({
 }) => {
   return (
     <div
-      className={`flex gap-6 items-center cursor-pointer heading-s bg-white dark:bg-darkBlueLight py-5 px-6 border-[3px] border-transparent rounded-[2rem] duration-500 items-shadow ${customClasses}`}
+      className={`flex gap-3 sm:gap-6 items-center cursor-pointer heading-s !text-[16px] sm:!text-2xl bg-white dark:bg-darkBlueLight py-3 px-3 sm:py-5 sm:px-6 border-[3px] border-transparent rounded-[2rem] duration-500 items-shadow ${customClasses}`}
     >
       {children}
     </div>
