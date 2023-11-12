1862d6d266209e93589666366bba039d97405090
Javier Guerrero <javigh87@gmail.com>
2023-11-12 09:49:59 +0100

    feature: Added MenuSelectionElement

--- src/components/SelectionElement/MenuSelectionElement.tsx
+++ src/components/SelectionElement/MenuSelectionElement.tsx
@@ -1,15 +1,38 @@
+import useAppStore from "../../store/useAppStore";
+
+import { Quizz } from "../../interfaces";
+
+import {
+  AccessibilityIcon,
+  CSSIcon,
+  HTMLIcon,
+  JSIcon,
+} from "../../assets/Icons";
+
 import SelectionElementContainer from "./SelectionElementContainer";
 
 interface Props {
-  icon: React.JSX.Element;
-  text: string;
+  quizz: Quizz;
 }
 
-const MenuSelectionElement: React.FC<Props> = ({ icon, text }) => {
+interface Icons {
+  [index: string]: React.JSX.Element;
+}
+
+const ICONS: Icons = {
+  HTML: <HTMLIcon />,
+  CSS: <CSSIcon />,
+  JavaScript: <JSIcon />,
+  Accessibility: <AccessibilityIcon />,
+};
+
+const MenuSelectionElement: React.FC<Props> = ({ quizz }) => {
+  const setGameSelected = useAppStore((state) => state.setGameSelected);
+
   return (
-    <SelectionElementContainer>
-      {icon}
-      <p className="flex-1 text-darkBlue dark:text-white">{text}</p>
+    <SelectionElementContainer onClick={() => setGameSelected(quizz)}>
+      {ICONS[quizz.title]}
+      <p className="flex-1 text-darkBlue dark:text-white">{quizz.title}</p>
     </SelectionElementContainer>
   );
 };
