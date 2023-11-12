84f22261b8eb71e3f540bea2ab0d1d7015bc70ef
Javier Guerrero <javigh87@gmail.com>
2023-11-12 06:00:21 +0100

    feature: Added SelectionElement to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./Button/Button";
+export { default as SelectionElement } from "./SelectionElement/SelectionElement";
 export { default as ThemeToggler } from "./ThemeToggler/ThemeToggler";
