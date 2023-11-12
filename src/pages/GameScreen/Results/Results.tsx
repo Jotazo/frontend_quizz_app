9f3213cf979d50cf26ff38df40dd9455d9d13611
Javier Guerrero <javigh87@gmail.com>
2023-11-12 20:57:42 +0100

    styles: Results desktop styles handled

--- src/pages/GameScreen/Results/Results.tsx
+++ src/pages/GameScreen/Results/Results.tsx
@@ -12,25 +12,27 @@ const Results = () => {
   return (
     <PageContainer>
       {" "}
-      <h1 className="sm:max-w-[85%] text-[40px] sm:text-7xl leading-tight text-darkBlue dark:text-white">
+      <h1 className="sm:max-w-[85%] lg:max-w-none lg:w-[520px] text-[40px] sm:text-7xl lg:text-6xl leading-tight text-darkBlue dark:text-white">
         Quiz completed{" "}
         <span className="font-[RubikMedium] font-bold">You scored...</span>
       </h1>
-      <section className="flex flex-col items-center bg-white dark:bg-darkBlueLight sm:gap-10 py-6 sm:py-12 mt-8 sm:mt-14 mb-4 sm:mb-8 rounded-xl sm:rounded-3xl">
-        <div className="heading-s flex items-center !text-[16px] sm:!text-2xl gap-4 transition duration-500">
-          {QUIZZ_ICONS[gameSelected!.title]}
-          <p className="flex-1 text-darkBlue dark:text-white">
-            {gameSelected!.title}
+      <section className="flex-1">
+        <section className="flex flex-col items-center bg-white dark:bg-darkBlueLight sm:gap-10 py-6 sm:py-12 mt-8 sm:mt-14 lg:mt-0 mb-4 sm:mb-8 rounded-xl sm:rounded-3xl">
+          <div className="heading-s flex items-center !text-[16px] sm:!text-2xl gap-4 transition duration-500">
+            {QUIZZ_ICONS[gameSelected!.title]}
+            <p className="flex-1 text-darkBlue dark:text-white">
+              {gameSelected!.title}
+            </p>
+          </div>
+          <h2 className="text-[88px] sm:text-9xl font-bold text-darkBlue dark:text-white">
+            {hits}
+          </h2>
+          <p className="text-custGrey dark:text-lightBlue sm:text-2xl">
+            out of {gameSelected?.questions.length}
           </p>
-        </div>
-        <h2 className="text-[88px] sm:text-9xl font-bold text-darkBlue dark:text-white">
-          {hits}
-        </h2>
-        <p className="text-custGrey dark:text-lightBlue sm:text-2xl">
-          out of {gameSelected?.questions.length}
-        </p>
+        </section>
+        <Button text="Play Again" onClick={onResetGame} />
       </section>
-      <Button text="Play Again" onClick={onResetGame} />
     </PageContainer>
   );
 };
--- src/pages/MainMenu/MainMenu.tsx
+++ src/pages/MainMenu/MainMenu.tsx
@@ -12,7 +12,7 @@ const MainMenu = () => {
       <section className="mb-10 sm:mb-14">
         <h1 className="sm:max-w-[75%] lg:max-w-none lg:w-[520px] text-[40px] sm:text-7xl lg:text-6xl leading-tight text-darkBlue dark:text-white lg:mb-10">
           Welcome to the{" "}
-          <span className="font-[RubikMedium] ">Frontend Quiz!</span>
+          <span className="font-[RubikMedium] font-bold">Frontend Quiz!</span>
         </h1>
         <ItalicText>Pick a subject to get started.</ItalicText>
       </section>
