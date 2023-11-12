9c365d70eea908064b7bd2dceb58deeff6835334
Javier Guerrero <javigh87@gmail.com>
2023-11-12 05:54:53 +0100

    feature: Added CorrectIcon & IncorrectIcon

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,2 +1,4 @@
+export { default as CorrectIcon } from "./CorrectIcon";
+export { default as IncorrectIcon } from "./IncorrectIcon";
 export { default as MoonIcon } from "./MoonIcon";
 export { default as SunIcon } from "./SunIcon";
