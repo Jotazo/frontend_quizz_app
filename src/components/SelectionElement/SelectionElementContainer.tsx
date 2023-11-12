c4b0d96d5e5d2f2573590d77e58bda5a9c83f743
Javier Guerrero <javigh87@gmail.com>
2023-11-12 09:47:55 +0100

    feature: Added HTMLProps to SelectionElementContainer

--- src/components/SelectionElement/SelectionElementContainer.tsx
+++ src/components/SelectionElement/SelectionElementContainer.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
+import { HTMLProps, ReactNode } from "react";
 
-interface Props {
+interface Props extends HTMLProps<HTMLDivElement> {
   children: ReactNode;
   customClasses?: string;
 }
@@ -8,10 +8,12 @@ interface Props {
 const SelectionElementContainer: React.FC<Props> = ({
   children,
   customClasses = "",
+  ...props
 }) => {
   return (
     <div
       className={`flex gap-3 sm:gap-6 items-center cursor-pointer heading-s !text-[16px] sm:!text-2xl bg-white dark:bg-darkBlueLight py-3 px-3 sm:py-5 sm:px-6 border-[3px] border-transparent rounded-2xl duration-500 items-shadow ${customClasses}`}
+      {...props}
     >
       {children}
     </div>
