cdffbd9b799d368f1f12923db1d43ad9a1c60ad9
Javier Guerrero <javigh87@gmail.com>
2023-11-12 21:42:14 +0100

    feature: Animation icons selection added

--- src/assets/Icons/IncorrectIcon.tsx
+++ src/assets/Icons/IncorrectIcon.tsx
@@ -1,6 +1,11 @@
+import { motion } from "framer-motion";
+
 const IncorrectIcon = () => {
   return (
-    <svg
+    <motion.svg
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
       xmlns="http://www.w3.org/2000/svg"
       className="w-6 sm:w-10 lg:w-6"
       fill="none"
@@ -10,7 +15,7 @@ const IncorrectIcon = () => {
         fill="#EE5454"
         d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z"
       />
-    </svg>
+    </motion.svg>
   );
 };
 
