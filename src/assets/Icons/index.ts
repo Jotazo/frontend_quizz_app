049ee1956a887cbbfaa4061da7facffb0ca5fb74
Javier Guerrero <javigh87@gmail.com>
2023-11-12 06:18:36 +0100

    feature: Added CSSIcon

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CorrectIcon } from "./CorrectIcon";
+export { default as CSSIcon } from "./CSSIcon";
 export { default as HTMLIcon } from "./HTMLIcon";
 export { default as IncorrectIcon } from "./IncorrectIcon";
 export { default as MoonIcon } from "./MoonIcon";
