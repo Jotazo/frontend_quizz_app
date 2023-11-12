116db52e8de34dbd7bb898cd93dbfa400e8eb91a
Javier Guerrero <javigh87@gmail.com>
2023-11-12 09:58:54 +0100

    fix: QUIZZ_ICONS imported

--- src/components/Header/GameSelectedElement.tsx
+++ src/components/Header/GameSelectedElement.tsx
@@ -1,8 +1,14 @@
-const GameSelectedElement = ({ icon }) => {
+import { QUIZZ_ICONS } from "../../utils/quizzIcons";
+
+interface Props {
+  quizzTitle: string;
+}
+
+const GameSelectedElement: React.FC<Props> = ({ quizzTitle }) => {
   return (
     <div className="heading-s flex items-center !text-[16px] sm:!text-2xl gap-4 transition duration-500">
-      {icon}
-      <p className="flex-1 text-darkBlue dark:text-white">HTML</p>
+      {QUIZZ_ICONS[quizzTitle]}
+      <p className="flex-1 text-darkBlue dark:text-white">{quizzTitle}</p>
     </div>
   );
 };
--- src/components/SelectionElement/MenuSelectionElement.tsx
+++ src/components/SelectionElement/MenuSelectionElement.tsx
@@ -2,12 +2,7 @@ import useAppStore from "../../store/useAppStore";
 
 import { Quizz } from "../../interfaces";
 
-import {
-  AccessibilityIcon,
-  CSSIcon,
-  HTMLIcon,
-  JSIcon,
-} from "../../assets/Icons";
+import { QUIZZ_ICONS } from "../../utils/quizzIcons";
 
 import SelectionElementContainer from "./SelectionElementContainer";
 
@@ -15,23 +10,12 @@ interface Props {
   quizz: Quizz;
 }
 
-interface Icons {
-  [index: string]: React.JSX.Element;
-}
-
-const ICONS: Icons = {
-  HTML: <HTMLIcon />,
-  CSS: <CSSIcon />,
-  JavaScript: <JSIcon />,
-  Accessibility: <AccessibilityIcon />,
-};
-
 const MenuSelectionElement: React.FC<Props> = ({ quizz }) => {
   const setGameSelected = useAppStore((state) => state.setGameSelected);
 
   return (
     <SelectionElementContainer onClick={() => setGameSelected(quizz)}>
-      {ICONS[quizz.title]}
+      {QUIZZ_ICONS[quizz.title]}
       <p className="flex-1 text-darkBlue dark:text-white">{quizz.title}</p>
     </SelectionElementContainer>
   );
