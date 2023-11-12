d42902f8382a3fc27d0f89c30f481fab091083fa
Javier Guerrero <javigh87@gmail.com>
2023-11-12 10:31:26 +0100

    feature: ItalicText added

---
+++ src/components/ItalicText/ItalicText.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const ItalicText: React.FC<Props> = ({ children }) => {
+  return (
+    <p className="font-[RubikItalic] text-custGrey dark:text-lightBlue mt-2 sm:text-2xl">
+      {children}
+    </p>
+  );
+};
+
+export default ItalicText;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from "./Button/Button";
 export { default as Header } from "./Header/Header";
 export { default as IconContainer } from "./IconContainer";
+export { default as ItalicText } from "./ItalicText/ItalicText";
 export { default as MenuSelectionElement } from "./SelectionElement/MenuSelectionElement";
 export { default as GameSelectionElement } from "./SelectionElement/GameSelectionElement";
 export { default as ThemeToggler } from "./ThemeToggler/ThemeToggler";
