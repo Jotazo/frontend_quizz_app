09fae3c0429c597d017ea0718507ff47d1e83d24
Javier Guerrero <javigh87@gmail.com>
2023-11-12 06:17:53 +0100

    fix: IconContainer customBg fixed

--- src/components/IconContainer.tsx
+++ src/components/IconContainer.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 interface Props {
   children: React.JSX.Element;
-  bgColor: string;
+  customBg: string;
 }
 
-const IconContainer: React.FC<Props> = ({ children, bgColor }) => {
-  return <div className={`bg-[${bgColor}] p-2 w-fit rounded-lg`}>{children}</div>;
+const IconContainer: React.FC<Props> = ({ children, customBg }) => {
+  return (
+    <div className={`p-2 w-fit rounded-lg ${customBg}`}>
+      {children}
+    </div>
+  );
 };
 
 export default IconContainer;
