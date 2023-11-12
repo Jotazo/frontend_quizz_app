d00934695083c9e48f5d70769b65090ee30832a3
Javier Guerrero <javigh87@gmail.com>
2023-11-12 19:24:33 +0100

    styles: App sm size updated

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 const App = () => {
   const gameSelected = useAppStore((state) => state.gameSelected);
   return (
-    <div className="w-full max-w-6xl mx-auto px-6 pt-5 sm:px-9 sm:pt-8 transition-all duration-500">
+    <div className="w-full max-w-6xl mx-auto px-6 pt-5 sm:px-16 sm:pt-12 transition-all duration-500">
       <Header />
       {gameSelected ? <GameScreen /> : <MainMenu />}
     </div>
