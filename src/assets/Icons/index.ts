a799bcdfa0e051005280f3c27f5264c4d2e40d1a
Javier Guerrero <javigh87@gmail.com>
2023-11-12 06:08:49 +0100

    feature: Added HTMLIcon

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CorrectIcon } from "./CorrectIcon";
+export { default as HTMLIcon } from "./HTMLIcon";
 export { default as IncorrectIcon } from "./IncorrectIcon";
 export { default as MoonIcon } from "./MoonIcon";
 export { default as SunIcon } from "./SunIcon";
