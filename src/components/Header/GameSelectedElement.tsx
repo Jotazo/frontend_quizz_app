2bceae2653ba0eb039cc4726253d0bc29f32c58c
Javier Guerrero <javigh87@gmail.com>
2023-11-12 10:15:21 +0100

    feature: GameSelectedElement header animated

--- src/components/Header/GameSelectedElement.tsx
+++ src/components/Header/GameSelectedElement.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 import { QUIZZ_ICONS } from "../../utils/quizzIcons";
 
 interface Props {
@@ -6,10 +8,19 @@ interface Props {
 
 const GameSelectedElement: React.FC<Props> = ({ quizzTitle }) => {
   return (
-    <div className="heading-s flex items-center !text-[16px] sm:!text-2xl gap-4 transition duration-500">
-      {QUIZZ_ICONS[quizzTitle]}
-      <p className="flex-1 text-darkBlue dark:text-white">{quizzTitle}</p>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={quizzTitle}
+        initial={{ x: -300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ y: -550, opacity: 0 }}
+        transition={{ duration: 0.2, ease: "easeInOut" }}
+        className="heading-s flex items-center !text-[16px] sm:!text-2xl gap-4 transition duration-500"
+      >
+        {QUIZZ_ICONS[quizzTitle]}
+        <p className="flex-1 text-darkBlue dark:text-white">{quizzTitle}</p>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
