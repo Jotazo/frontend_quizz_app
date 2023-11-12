5b9d19290426cf0bbc510f3f1a499074725a8589
Javier Guerrero <javigh87@gmail.com>
2023-11-12 18:18:08 +0100

    feature: GameSelectionElement finished

--- src/components/SelectionElement/GameSelectionElement.tsx
+++ src/components/SelectionElement/GameSelectionElement.tsx
@@ -1,4 +1,5 @@
 import { HTMLProps } from "react";
+
 import { CorrectIcon, IncorrectIcon } from "../../assets/Icons";
 
 import SelectionElementContainer from "./SelectionElementContainer";
@@ -21,20 +22,26 @@ const GameSelectionElement: React.FC<Props> = ({
   isSubmited,
   ...props
 }) => {
-  const isIncorrect = isSubmited && isSelected && !isCorrect;
+  const isIncorrectSubmited = isSubmited && isSelected && !isCorrect;
+  const isCorrectSubmited =
+    (isSubmited && isCorrect) || (isSubmited && isSelected && isCorrect);
+
+  const styleSelected = isSelected ? "selected" : "";
+  const styleCorrect = isCorrectSubmited ? "correct" : "";
+  const styleIncorrect = isIncorrectSubmited ? "incorrect" : "";
+  const styleDisabled = isSubmited ? "pointer-events-none" : "";
+
   return (
     <SelectionElementContainer
-      customClasses={`game-selection-element ${isSelected && "selected"} ${
-        isCorrect && "correct"
-      } ${isIncorrect && "incorrect"}`}
+      customClasses={`game-selection-element ${styleSelected} ${styleCorrect} ${styleIncorrect} ${styleDisabled}`}
       {...props}
     >
       <span className="bg-lightGrey p-3 px-4 sm:p-4 sm:px-6 rounded-lg text-custGrey transition duration-500">
         {letter}
       </span>
       <p className="flex-1 text-darkBlue dark:text-white">{text}</p>
-      {isCorrect && <CorrectIcon />}
-      {isIncorrect && <IncorrectIcon />}
+      {isCorrectSubmited && <CorrectIcon />}
+      {isIncorrectSubmited && <IncorrectIcon />}
     </SelectionElementContainer>
   );
 };
