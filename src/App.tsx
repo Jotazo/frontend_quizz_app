1e76aa9dee6296816efbf024369ec6c9fbbb1cbd
Javier Guerrero <javigh87@gmail.com>
2023-11-12 18:17:31 +0100

    feature: App updated with GameScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { Header } from "./components";
+
+import useAppStore from "./store/useAppStore";
+
+import { MainMenu, GameScreen } from "./pages";
+
+import "./App.css";
 
 const App = () => {
-
+  const gameSelected = useAppStore((state) => state.gameSelected);
   return (
-    <>
-      HEY
-    </>
-  )
-}
+    <div className="w-full max-w-6xl mx-auto px-6 pt-5 sm:px-9 sm:pt-8 transition-all duration-500">
+      <Header />
+      {gameSelected ? <GameScreen /> : <MainMenu />}
+    </div>
+  );
+};
 
-export default App
+export default App;
