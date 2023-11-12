96fd8999bc9d422369fd9c44f22af45582b5a676
Javier Guerrero <javigh87@gmail.com>
2023-11-12 10:35:15 +0100

    feature: PageContainer added to MainMenu

--- src/pages/MainMenu/MainMenu.tsx
+++ src/pages/MainMenu/MainMenu.tsx
@@ -1,12 +1,16 @@
 import useAppStore from "../../store/useAppStore";
 
-import { ItalicText, MenuSelectionElement } from "../../components";
+import {
+  ItalicText,
+  MenuSelectionElement,
+  PageContainer,
+} from "../../components";
 
 const MainMenu = () => {
   const quizzes = useAppStore((state) => state.quizzes);
 
   return (
-    <div className="mt-14">
+    <PageContainer>
       <section className="mb-10 sm:mb-14">
         <h1 className="sm:max-w-[75%] text-[40px] sm:text-7xl leading-tight text-darkBlue dark:text-white">
           Welcome to the{" "}
@@ -19,7 +23,7 @@ const MainMenu = () => {
           <MenuSelectionElement key={quizz.title} quizz={quizz} />
         ))}
       </ul>
-    </div>
+    </PageContainer>
   );
 };
 
